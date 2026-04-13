53f232b61a01b7313e3b4dae7cb3780e0bfbc3d0
ikbalrahmat <ikbalrahmattaupil26@gmail.com>
2026-04-13 19:07:28 +0700

    Perbaikan UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,14 +13,14 @@ class UserSeeder extends Seeder
         User::create([
             'nama' => 'Super Admin',
             'email' => 'superadmin@sitor.com',
-            'password' => 'admin123', // Cukup teks biasa, Model akan otomatis men-hash
+            'password' => 'Admin123!', // Cukup teks biasa, Model akan otomatis men-hash
             'jabatan' => 'Kepala SPI',
             'instansi' => 'Kantor Pusat',
             'unit_kerja' => 'Satuan Pengawasan Intern',
             'role' => 'Super Admin',
             'status_keaktifan' => true,
             // Bypass security check untuk Super Admin perdana
-            'is_first_login' => false, 
+            'is_first_login' => false,
             'password_changed_at' => Carbon::now(), // Dianggap baru ganti password hari ini
             'failed_login_attempts' => 0,
             'is_locked' => false,
